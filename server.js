8756cf1a8fc8bd553e7172b39a2f606e724d0750
Austin Melms <memyselfandlie@gmail.com>
2018-05-29 17:07:20 -0400

    Request Header Parser Microservice

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 let path = require('path')
 let app = express()
-
+app.set('trust proxy', true)
 app.set('port', 80)
 
 app.use(express.static(path.join(__dirname, '../personal-site')))
+
+app.get('/whoami', async (req, res) => {
+  let whoAmI = {
+    'ipaddress': req.ip,
+    'language': req.headers['accept-language'].split(',')[0],
+    'software': req.headers['user-agent'].match(/\((.*?)\)/)[1]
+  }
+  console.log(whoAmI)
+  return res.json(whoAmI)
+})
 
 app.use(function (req, res, next) {
   res.status(404)
