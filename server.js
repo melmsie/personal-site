ed356cb906369944bfd2ed97b52672181e0bf577
Austin Melms <memyselfandlie@gmail.com>
2018-05-29 17:32:43 -0400

    geo p2

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ const config = require('./config.json')
 app.use(express.static(path.join(__dirname, '../personal-site')))
 
 app.get('/whoami', async (req, res) => {
-  let location = await snek.get(`ipinfo.io/${req.ip}/geo?token=${config.ipToken}`)
+  let location = await snek.get(`https://ipinfo.io/${req.ip}/geo?token=${config.ipToken}`)
+  location = location.body
   let whoAmI = {
     'ipaddress': req.ip,
     'location': `${location.country}: ${location.region}`,
     'language': req.headers['accept-language'].split(',')[0],
     'software': req.headers['user-agent'].match(/\((.*?)\)/)[1]
   }
-  console.log(location)
+  console.log(location.body)
   return res.json(whoAmI)
 })
 
