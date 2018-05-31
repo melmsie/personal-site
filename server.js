9d8e41f6b44f28398500333109c0ba27b9dc5a6e
Austin Melms <memyselfandlie@gmail.com>
2018-05-31 00:29:41 -0400

    added TODO's and FIXME's

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ app.set('trust proxy', true)
 app.set('port', 80)
 const snek = require('snekfetch')
 const config = require('./config.json')
-
+/*
+  // TODO: add an about page at about.melmsie.com [markdown?]
+  // TODO: add a "see my tech stack" page at stack.melmsie.com [markdown?]
+  // TODO: add a fully authed kid counter page at kwc.melmsie.com [MongoDB?]
+*/
 app.use(express.static(path.join(__dirname, '../personal-site')))
 
 app.get('/whoami', async (req, res) => {
+  // TODO: Make this pretty
   let location = await snek.get(`https://ipinfo.io/${req.ip}/geo?token=${config.ipToken}`)
   location = location.body
   let whoAmI = {
@@ -20,11 +25,15 @@ app.get('/whoami', async (req, res) => {
   return res.json(whoAmI)
 })
 
-app.get('/date', async (req, res) => {
+app.get('/date', async (req, res) => { // TODO: [Markdown?]
   return res.send('Send a Unix timestamp, or natural language date to "/date/" and receive the opposite JSON reponse.')
 })
 
 app.get('/date/*', async (req, res) => {
+  /*
+  TODO: Add timezone options (as well as time with the date)
+  FIXME: Make sure input is validated
+  */
   let pre
   let converted
   let input = decodeURI(req.url).split('/')[2]
@@ -47,6 +56,7 @@ app.get('/short/*', async (req, res) => {
 })
 
 app.use(function (req, res, next) {
+  // TODO: 404 page when
   res.status(404)
   res.send('404: Page Not Found')
 })
